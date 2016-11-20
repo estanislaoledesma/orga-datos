9072a037e72e2a2e767bcd2f6537d4bf4ef4d968
martinqueija <tinqueija@gmail.com>
2016-11-20 20:25:18 -0300

    Update LSH3.py

--- LSH3.py
+++ LSH3.py
@@ -10,9 +10,7 @@
 #https://nickgrattan.wordpress.com/2014/03/03/lsh-for-finding-similar-documents-from-a-large-number-of-documents-in-c/
 #https://github.com/chrisjmccormick/MinHash/blob/master/runMinHashExample.py
 
-
-import numpy as np
-from random import randint, seed, choice, random
+from random import randint, random
 import math
 from pyspark import SparkContext, SparkConf
 import string
@@ -27,40 +25,32 @@ maxint = sys.maxint
 bandas = 10
 hashesPorBanda = 4
 cantHashes = bandas * hashesPorBanda
-nGram = 3
-shingleaWords = True
-lineasEnLearn = 1001
-maxCantCodigosBandas = bandas * lineasEnLearn
+nGram = 15
+xorHash = True
+shingleaWords = False
+escribeScores = False
+
+#lineasEnLearn = 300000					#Big Dataset Learn File
+lineasEnLearn = 227380 #TrainKaggle00
+
+#cantReviewsToPredict = 73680000
+cantReviewsToPredict = 227380 #TrainKaggle01
+#cantReviewsToPredict = 113691 #kaggleTestFile
+hashDisplaces = []
 
 valoresPorBanda = dict()
 
-def log(text):
-	with open("/media/tino/Tera/bigdata/log.txt", "a") as myfile:
-		myfile.write(text + "\n")
-
-log("\n\n" + str(datetime.datetime.now()))
-log("-------------LOG---------------")
-log("Bandas: " + str(bandas))
-log("Hashes por banda: " + str(hashesPorBanda))
-log("Cantidad de hashes: " + str(hashesPorBanda * bandas))
-if (shingleaWords):
-	log("Shinglea por palabras con K-Grams: " + str(nGram))
-else:
-	log("Shinglea por caracteres con K-Grams: " + str(nGram))
-log(" ")
+rutaTestFile = "/home/tino/Documents/trainKaggle01.csv"
+rutaLearnFile = "/home/tino/Documents/trainKaggle00.csv"
+rutaScores = "/home/tino/Documents/kagglePredictions.csv"
 
 
 
-
-hashDisplaces = []
-for x in range(cantHashes):
-	hashDisplaces.append(int(random()))
-	#hashDisplaces.append(0)
-
-hashDisplacesNuevo = []
-for x in range(cantHashes):
-	hashDisplacesNuevo.append((int(random()), int(random())))
-
+#
+#
+#	Funciones
+#
+#
 def esPrimo(n):
 	for i in range(3, n):
 		if n % i == 0:
@@ -73,85 +63,89 @@ def damePrimoSiguiente(numero):
 		numero = numero + 1
 	return numero
 
-primoC = damePrimoSiguiente(maxCantCodigosBandas)
+primoC = damePrimoSiguiente(bandas * lineasEnLearn)
 
-def hashInt(integer,indice):
-	a = hashDisplacesNuevo[indice][0]
-	b = hashDisplacesNuevo[indice][1]
-	return ((a*integer)+b) % primoC
+def log(text):
+	with open("/media/tino/Tera/bigdata/log.txt", "a") as myfile:
+		myfile.write(text + "\n")
 
-
-
-
-
-def dame_shingles_chars(texto, cantidadChars):
-
-	return [texto[i:i + cantidadChars] for i in range(len(texto) - cantidadChars + 1)]
-
-def dame_shingles(texto, nGram, maxLargoPalabra):
-
-	
+def logInit():
+	log("\n\n" + str(datetime.datetime.now()))
+	log("-------------LOG---------------")
+	log("Learn file: " + rutaLearnFile)
+	log("Reviews to learn: " + str(lineasEnLearn))
+	log("Test file: " + rutaTestFile)
+	log("Bandas: " + str(bandas))
+	log("Bandas calculadas: " + str(bandas * lineasEnLearn))
+	log("XOR: " + str(not xorHash))
+	log("Hashes por banda: " + str(hashesPorBanda))
+	log("Cantidad de hashes: " + str(hashesPorBanda * bandas))
 	if (shingleaWords):
-#Devuelve una lista con los shingles de texto, procesado palabra por palabra, donde los mismos tienen un 
-#tamano de nGram y un maximo de largo de palabra igual a maxLargoPalabra.'''
+		log("Shinglea por palabras con K-Grams: " + str(nGram))
+	else:
+		log("Shinglea por caracteres con K-Grams: " + str(nGram))
+	log(" ")
+
+#
+#	generateRandoms()
+#	Genera una lista de numeros semi-aleatorios, con estos se XORea la funcion de Hash de python para obtener variantes de la funcion de hash.
+#
+def generateRandoms():
+	randoms = []
+	for x in range(cantHashes):					
+		randoms.append(int(randint(0,maxint)))
+	return randoms
+
+
+#
+#	dameShingles()
+#	Si shingleaWords = True, devuelve una lista de listas. Cada subLista contiene nGram cantidad de palabras (< maxLargoPalabra) por shingle.
+#	si shingleaWords = False, devuelve una lista de shingles de nGram caracteres.
+#
+def dameShingles(texto, nGram, maxLargoPalabra):
+	if (shingleaWords):
 		texto = texto.split()
 		return [texto[i:i+nGram] for i in range(len(texto) - nGram + 1) if len(texto[i]) < maxLargoPalabra]
 	else:
-#Devuelve una lista con los shingles de texto, procesado caracter por caracter, donde los mismos tienen
-#un tamano de nGram.'''
+		return [texto[i:i + nGram] for i in range(len(texto) - nGram + 1)]	
 
-		return [texto[i:i + nGram] for i in range(len(texto) - nGram + 1)]		
-
-def dame_minhashes_shingles2(shingles):
-	'''Devuelve una lista de minhashes para la lista de shingles pasada por parametro, utiliza hash and displace.'''
+	
+#
+#	dameMinhashShingles()
+#	Devuelve la lista de minhashes para la lista de shingles por caracteres o palabra.
+#	Si xorHash = True, el displace random para la funcion hash() se XOrea, de lo contrario se suma.
+#
+def dameMinhashShingles(shingles):
 	minhashes = []
 	for i in range(cantHashes):
 		minhash = maxint
 		esteHash = 0
 		if shingleaWords:
 			for conjunto in shingles:
-				#for shingle in conjunto:
-				#	esteHash += abs(hash(shingle) + hashDisplaces[i])
-				
-
-				#esteHash = abs(hash(" ".join(conjunto)) + hashDisplaces[i])
-				
-				esteHash = abs(hashInt(hash(" ".join(conjunto)),i))				
-
+				if xorHash:
+					esteHash = abs(hash(" ".join(conjunto)) ^ hashDisplaces[i])
+				else :
+					esteHash = abs(hash(" ".join(conjunto)) + hashDisplaces[i])
 				if esteHash < minhash:
 					minhash = esteHash
 				esteHash = 0
 		else:
 			for shingle in shingles:
-				esteHash = abs(hash(shingle) + hashDisplaces[i])
+				if xorHash:
+					esteHash = abs(hash(shingle) ^ hashDisplaces[i])
+				else:
+					esteHash = abs(hash(shingle) + hashDisplaces[i])
 				if esteHash < minhash:
 					minhash = esteHash
 		minhashes.append(minhash)
 	return minhashes
 
-
-
-def dame_hash_bandasNumpy(minhashes):
-	'''Devuelve una lista de hashes de banda a partir de una lista de minhashes en la cual hay un numero de bandas
-	y un numero de hashesPorBanda para la misma.'''
-	a = np.zeros(bandas)
-	codigosBandas = []
-	hashBanda = 0
-	intervalo = 0
-	indiceArreglo = 0
-	for i in range(cantHashes):
-		intervalo = intervalo + 1
-		if (intervalo == hashesPorBanda):
-			a[indiceArreglo] = hashBanda
-			indiceArreglo = indiceArreglo + 1
-			hashBanda = 0
-			intervalo = 0
-		hashBanda += hash(minhashes[i])
-	return a
-
-def dame_hash_bandas(minhashes):
-	'''Devuelve una lista de hashes de banda a partir de una lista de minhashes en la cual hay un numero de bandas
-	y un numero de hashesPorBanda para la misma.'''
+#
+#	dameHashBandas()
+#	Acumula los hashes de los minhashes de acuerdo a la cantidad de Bandas y hashesPorBandas.
+#	Si xorHash = True, el displace random para la funcion hash() se XOrea, de lo contrario se suma.
+#
+def dameHashBandas(minhashes):
 	codigosBandas = []
 	hashBanda = 0
 	intervalo = 0
@@ -165,19 +159,23 @@ def dame_hash_bandas(minhashes):
 			intervalo = 0
 	return codigosBandas
 
-
-def flatmapeo(idReview, puntaje, array):
-	'''Devuelve una lista donde cada posicion es una tupla donde la primera posicion es idReview, la segunda
-	puntaje y la tercera, domde el valor de la misma posicion en array.'''
+#
+#	myFlatMap()
+#	Para un review, un puntaje y un array de claves de bandas:
+#	Genera tuplas del estilo: (idReview, Puntaje, claveBanda)
+#	
+def myFlatMap(idReview, puntaje, array):
 	lista = []
 	for x in array:
 		lista.append((idReview,puntaje,x))
 	return lista
 
-
+#
+#	damePromedioBucket()
+#	Para una clave de Bandas, busca en el diccionario y 
+#	devuelve el promedio de los scores que se encuentran en ese bucket.
+#	
 def damePromedioBucket(indiceDict):
-	'''Devuelve el promedio del bucket dado por la clave indiceDict en dicto (un diccionario), donde el valor
-	de la misma es una lista de enteros. En caso de error, devuelve 0.'''
 	count = 0
 	acum = 0
 	try:
@@ -185,16 +183,20 @@ def damePromedioBucket(indiceDict):
 			count += 1
 			acum += float(i)
 		if (count != 0):
-			return acum / count
+			return float(acum / float(count))
 		else:
 			return 0
 	except:
 		return 0
 
+
+
+#
+#	calcScore()
+#	Para una lista de claves de Bandas, consulta los promedios para cada una
+#	y devuelve el promedio de los promedios de los buckets.
+#	
 def calcScore(bandas):
-	'''Devuelve el promedio del promedio de cada banda en bandas (lista de bandas), donde el primer promedio
-	se calcula mediante damePromedioBucket de la banda (en el diccionario dicto), y el segundo es un promedio
-	de todos esos promedios. En caso de error, devuelve -1.'''
 	acum = 0
 	count = 0
 	for unaBanda in bandas:
@@ -203,86 +205,96 @@ def calcScore(bandas):
 			count += 1
 		acum += promedioBucket
 	if (acum == 0):
-		return -1
+		return 2.5
 	else:
-		return acum / count
+		promedioBuckets = acum / count
+		if (promedioBuckets < 1):
+			promedioBuckets = 1
+		return promedioBuckets
 
 
+def myMap(predictedScore,actualScore):
+	if predictedScore == -1:
+		return 0
+	else:
+		return abs(predictedScore - actualScore)
 
+
+#
+#	
+#	Plain Locality Sensitive Hashing
+#
+#
+
+
+logInit()
+hashDisplaces = generateRandoms()
 sc = SparkContext(conf = SparkConf())
-learn = sc.textFile('/media/tino/Tera/bigdata/parsedTrainSmall.csv',8)
-lineasEnLearn = learn.count()
-learn = learn.map(lambda x: x.split('|')).map(lambda x: (x[0],x[2], dame_minhashes_shingles2(dame_shingles(x[1],nGram,500))))
+learn = sc.textFile(rutaLearnFile,8)
 
-#from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
-#from pyspark.mllib.regression import LabeledPoint
-#from pyspark.mllib.linalg import DenseVector
-
-#data_for_decision_tree = learn.map(lambda x: LabeledPoint(label = x [1], features = DenseVector(x[2])))
-#(dataTrain, dataTest) = data_for_decision_tree.randomSplit([0.7, 0.3])
-#model = DecisionTree.trainRegressor(dataTrain, categoricalFeaturesInfo={}, impurity='variance', maxDepth=5, maxBins=32)
-#predictions = model.predict(dataTest.map(lambda x: x.features))
-#labelsAndPredictions = dataTest.map(lambda x: x.label).zip(predictions)
-#testMSE = labelsAndPredictions.map(lambda (v, p): (v - p) * (v - p)).sum() / float(dataTest.count())
-#log("MSE = %f"%(testMSE))
-
-learn = learn.map(lambda x: (x[0],x[1], dame_hash_bandas(x[2])))
-learn = learn.flatMap(lambda x: flatmapeo(x[0], x[1], x[2])) #(u'a9wx8dk93sn5', u'1.0', 813759583895638922)
+learn = learn.map(lambda x: x.split('|'))
+	#(ID, ReviewText, Score)
+learn = learn.map(lambda x: (x[0],x[2], dameShingles(x[1],nGram,500)))
+	#(ID, Score, [Shingles])
+learn = learn.map(lambda x: (x[0],x[1], dameMinhashShingles(x[2])))
+	#(ID, Score, [minHashShingles])
+learn = learn.map(lambda x: (x[0],x[1], dameHashBandas(x[2])))
+	#(ID, Score, [clavesBandas])
+learn = learn.flatMap(lambda x: myFlatMap(x[0], x[1], x[2])) 
+	#(ID, Score, claveBanda)
 learn = learn.map(lambda x: (x[2], x[1]))
-cantBandasCalculadas = learn.count()
-data = learn.collect()
+	#(claveBanda, Score)
+data = learn.collect()						#Non Lazy Operation
 
-for a in data:
-	valoresPorBanda.setdefault(a[0], [])
-	valoresPorBanda[a[0]].append(a[1])
-	
-#print valoresPorBanda
-#flatmapeado = learn.count()
-#learn = learn.groupByKey()
-#agrupado = learn.count()
-#learn = learn.filter(lambda x: len(x[1].data) >= minClusterSize)
-#filtrado = learn.count()
-#dicto = learn.collectAsMap()
+print("\n\n\n\n\nEMPIEZA DICCIONARIO\n\n\n\n\n")
+for learnedReview in data:
+	valoresPorBanda.setdefault(learnedReview[0], [])			#Establece el formato del diccionario
+	valoresPorBanda[learnedReview[0]].append(float(learnedReview[1]))	#Agrega el valor del score de learnedReview al diccionario
+print("\n\n\n\nTERMINA DICCIONARIO\n\n\n\n\n")
 
-try:
-	shutil.rmtree('finalout.csv')
-except:
-	print "Folder does not exist."
-
-#learn.saveAsTextFile('/media/tino/Tera/bigdata/finalout.csv')
-
-
-
+log("CLAVES EN EL DICC: " + str(len(valoresPorBanda)))
 
 
 #
 #	Comienza la prediccion
 #
+print("\n\n\n\COMIENZA PREDICCION\n\n\n\n\n")
 
-
-test = sc.textFile('/media/tino/Tera/bigdata/parsedTestSmall.csv',8)
-test = test.map(lambda x: x.split('|')).map(lambda x: (x[0],x[2], dame_hash_bandas(dame_minhashes_shingles2(dame_shingles(x[1],nGram,500)))))
+test = sc.textFile(rutaTestFile,8)
+test = test.map(lambda x: x.split('|'))
+	#(ID, ReviewText, Score)
+test = test.map(lambda x: (x[0],x[2], dameShingles(x[1],nGram,500)))
+	#(ID, Score, [Shingles])
+test = test.map(lambda x: (x[0],x[1], dameMinhashShingles(x[2])))
+	#(ID, Score, [MinHashes])
+test = test.map(lambda x: (x[0],x[1], dameHashBandas(x[2])))
+	#(ID, Score, [ClavesBandas])
 test = test.map(lambda x: (x[0],x[1],calcScore(x[2])))
-cantReviewsToPredict = test.count()
+	#(ID, Score, predictedScore)
 test = test.filter(lambda x: x[2] != -1)
-cantReviewsPredicted = test.count()
-
-test = test.map(lambda x: abs(float(x[2])-float(x[1])))
-test = test.map(lambda x: (x*x))
-number = test.reduce(lambda x,y: x+y)
-number = number / cantReviewsPredicted
 
 
-try:
-	shutil.rmtree('scores.csv')
-except:
-	print "Folder does not exist."
-#test.saveAsTextFile('scores.csv')
-log("LINEAS EN EL LEARN: " + str(lineasEnLearn))
-log("CLAVES EN EL DICC: " + str(len(valoresPorBanda)))
-log("BANDAS CALCULADAS: " + str(cantBandasCalculadas))
+if escribeScores:
+	data = test.collect()
+	f = open(rutaScores, 'w')
+	for pred in data:
+			linea = str(pred[2]) + "," + str(pred[0]) + "\n"
+			f.write(linea)
+
+print("\n\n\n\CHECKPOINT\n\n\n\n\n")
+
+
+#cantReviewsPredicted = test.count()		#Non Lazy Operation
+cantReviewsPredicted = cantReviewsToPredict
+
+log("RELACION: " + str(float(float(cantReviewsPredicted) / float(cantReviewsToPredict))))
 log("REVIEWS A PREDECIR: " + str(cantReviewsToPredict))
 log("REVIEWS CON SCORE: " + str(cantReviewsPredicted))
-log("RELACION: " + str(float(float(cantReviewsPredicted) / float(cantReviewsToPredict))))
-log("PROMEDIO DE LOS CUADRADOS DE LAS DIFERENCIAS: " + str(number))
 
+test = test.map(lambda x: abs(float(x[2])-float(x[1])))
+#test = test.map(lambda x: myMap(float(x[2]),float(x[1])))
+test = test.map(lambda x: (x*x))
+number = test.reduce(lambda x,y: x+y)		#Non Lazy Operation
+number = number / cantReviewsPredicted
+
+log("PROMEDIO DE LOS CUADRADOS DE LAS DIFERENCIAS: " + str(number))
